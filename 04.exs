8d8ad947c1bebd9a671bf9f584dd4523c553dfed
William Huba <hexedpackets@gmail.com>
2025-12-04 09:49:20 -0500

    Day 4

---
+++ 04.exs
@@ -0,0 +1,66 @@
+Mix.install([:nx])
+
+defmodule Solution do
+  def create_tensor(data) do
+    data
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn row ->
+      row
+      |> to_charlist()
+      |> Enum.map(fn ?. -> 0; ?@ -> 1 end)
+    end)
+    |> Nx.tensor()
+  end
+
+  def find_rolls(t) do
+    t
+    |> Nx.window_sum({3, 3}, padding: :same)
+    |> Nx.less_equal(4)
+    |> Nx.logical_and(t)
+  end
+
+  def find_and_remove_rolls(t, total \\ 0) do
+    next = find_rolls(t)
+    count = count_rolls(next)
+    if count > 0 do
+      t
+      |> Nx.logical_xor(next)
+      |> find_and_remove_rolls(total + count)
+    else
+      total
+    end
+  end
+
+  def count_rolls(t) do
+    t
+    |> Nx.sum()
+    |> Nx.to_number()
+  end
+end
+
+_example = """
+..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.
+"""
+
+t = File.read!("input/04.txt")
+|> Solution.create_tensor()
+
+IO.write("Part 1: ")
+t
+|> Solution.find_rolls()
+|> Solution.count_rolls()
+|> IO.puts()
+
+IO.write("Part 2: ")
+t
+|> Solution.find_and_remove_rolls()
+|> IO.puts()
